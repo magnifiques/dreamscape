ecba7ea2a161c274306ddeb35b7f32428c61eddb
Arpit <arpit.vaghela@outlook.com>
2024-07-14 00:41:58 -0400

    add initial setup code

---
+++ components/Navbar.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <Link href="/">
+        <p className="blue-gradient_text">AV</p>
+      </Link>
+      <nav className="flex text-lg gap-7 font-medium">
+        <Link
+          href="/about"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          About
+        </Link>
+        <Link
+          href="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "text-black"
+          }
+        >
+          Projects
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.glb$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            outputPath: "static/assets/",
+            publicPath: "/_next/static/assets/",
+            name: "[name].[hash].[ext]",
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
