f6e6cb7c1324976780e8628cbcab043ebeeee015
Arpit <arpit.vaghela@outlook.com>
2024-07-14 23:34:23 -0400

    implement 3d models on home page

--- app/page.js
+++ app/page.js
@@ -2,15 +2,20 @@
 
 import Navbar from "@/components/Navbar";
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import Loader from "@/components/Loader";
 import Island from "@/models/Island";
 import Sky from "@/models/Sky";
+import Plane from "@/models/Plane";
+import Bird from "@/models/Bird";
 
 export default function Home() {
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
+
   const adjustIslandScreenSize = () => {
     let screenScale = null;
-    let screenPosition = [0, -11.5, -37];
+    let screenPosition = [0, -6.5, -30];
     let rotation = [0.1, 4.7, 0];
 
     if (window.innerWidth < 768) {
@@ -24,6 +29,25 @@ export default function Home() {
 
   const [isLandPosition, islandScale, islandRotation] =
     adjustIslandScreenSize();
+
+  const adjustPlaneScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = null;
+    // let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -2, -1];
+    }
+
+    return [screenPosition, screenScale];
+  };
+
+  const [planePosition, planeScale] = adjustPlaneScreenSize();
+
   return (
     <main className="bg-slate-300/20">
       <Navbar />
@@ -32,7 +56,9 @@ export default function Home() {
           POPUP
         </div> */}
         <Canvas
-          className="w-full h-screen bg-transparent"
+          className={`w-full h-screen bg-transparent ${
+            isRotating ? "cursor-grabbing" : "cursor-grab"
+          }`}
           camera={{
             near: 0.1,
             far: 1000,
@@ -46,11 +72,21 @@ export default function Home() {
               groundColor="#000000"
               intensity={1}
             />
-            <Sky />
+            <Sky isRotating={isRotating} />
+            <Bird />
+            <Plane
+              isRotating={isRotating}
+              position={planePosition}
+              scale={planeScale}
+              rotation={[0, Math.PI * 0.5, 0]}
+            />
             <Island
               position={isLandPosition}
               scale={islandScale}
               rotation={islandRotation}
+              isRotating={isRotating}
+              setCurrentStage={setCurrentStage}
+              setIsRotating={setIsRotating}
             />
           </Suspense>
         </Canvas>
---
+++ models/Plane.jsx
@@ -0,0 +1,25 @@
+"use client";
+import React, { useEffect, useRef } from "react";
+import planeScene from "../assets/3d/plane.glb";
+import { useAnimations, useGLTF } from "@react-three/drei";
+
+const Plane = ({ isRotating, ...props }) => {
+  const plane = useGLTF(planeScene);
+  const ref = useRef();
+  const { actions } = useAnimations(plane.animations, ref);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions["Take 001"].play();
+    } else {
+      actions["Take 001"].stop();
+    }
+  }, [actions, isRotating]);
+  return (
+    <mesh {...props} ref={ref}>
+      <primitive object={plane.scene} />
+    </mesh>
+  );
+};
+
+export default Plane;
